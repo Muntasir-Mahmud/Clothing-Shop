949dbfe64d3a68c148863a76b6b5865afa4dd417
Muntasir Mahmud <m.siyam529@gmail.com>
2021-03-13 19:10:07 +0600

    Added homepage component with styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
