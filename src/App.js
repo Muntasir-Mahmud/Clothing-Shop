22106553b61a2ea8cc6721c4254b7856c9cdbae2
Muntasir Mahmud <m.siyam529@gmail.com>
2021-03-16 21:42:07 +0600

    Added sign in form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndOut from "./pages/sign-in-and-up/sign-in-and-up.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndOut} />
       </Switch>
     </div>
   );
